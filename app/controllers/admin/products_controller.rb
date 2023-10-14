61a9d57134a7d1fbe616f46264522566599b4b8f
okuyama-code <130431374+okuyama-code@users.noreply.github.comm>
2023-10-15 08:02:11 +0900

    productsの管理者側での実装

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,44 @@
+class Admin::ProductsController < ApplicationController
+  # 商品を閲覧・作成・編集する機能はログイン済の管理者のみ権限を付与する形にしますので、authenticate_admin! を記述しました。authenticate_admin! の admin はモデル名になります。
+  before_action :authenticate_admin!
+  before_action :set_product, only: %i[show edit update]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock, :image)
+  end
+end
